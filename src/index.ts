338affdd5e4c6abb12ea65080021a8dab96d667d
Silverfeelin <Silverfeelin@gmail.com>
2025-11-16 20:14:12 +0100

    Clean exports

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './helpers/node-helper.js';
 export * from './helpers/spirit-tree-helper.js';
 
 // Export all interface types
+export * from './interfaces/item.interface.js';
 export type * from './interfaces/sky-data.interface.js';
 export type * from './interfaces/area.interface.js';
 export type * from './interfaces/base.interface.js';
@@ -12,14 +13,13 @@ export type * from './interfaces/calculator-data.interface.js';
 export type * from './interfaces/cost.interface.js';
 export type * from './interfaces/event.interface.js';
 export type * from './interfaces/iap.interface.js';
-export * from './interfaces/item.interface.js';
-export type * from './interfaces/item.interface.js';
 export type * from './interfaces/item-list.interface.js';
 export type * from './interfaces/map-data.interface.js';
 export type * from './interfaces/map-shrine.interface.js';
 export type * from './interfaces/node.interface.js';
 export type * from './interfaces/realm.interface.js';
 export type * from './interfaces/special-visit.interface.js';
+export type * from './interfaces/special-visit-spirit.interface.js';
 export type * from './interfaces/season.interface.js';
 export type * from './interfaces/shop.interface.js';
 export type * from './interfaces/spirit.interface.js';
